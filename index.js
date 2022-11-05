6cbbfe3d036e1ba4203cca4e64dc450369a3cb32
Jithendar reddy <94469047+Jithendar9676@users.noreply.github.com>
2022-11-05 08:28:21 +0000

    update

--- index.js
+++ index.js
@@ -3,24 +3,27 @@
 let number=Math.floor(Math.random()*10+1);
 console.log("Welcome to the GUSSEING GAME! \n Enter the numbers between 1-10. \n\t *** Lets Start ****")
 
-let count=0;
+let score=10;
 let a;
 do{
   a=prompt("Enter your Gussed Number :")
   a=Number.parseInt(a)
-  
-  if(a<number){
+  score -=1;
+  if(a<number && a>0){
     console.log("Sorry!\t Your Gusse is Low Then the actual Number.\n Try again;")
 
-  }else if(a>number){
+  }else if(a>number && a<11){
     console.log("Sorry!\t Your Gusse is High Then the actual Number.\n Try again;")
-  }else{
+  }else if(a==number){
   console.log("Hurry!!! \t Your Gusse is Correct: " +number)
+    console.log("\n You gussed the number in " + `${10-score}` + " Chances")
+  }else{
+    console.log("!Offo!...Enter the number between 1 to 10")
   }
-  count++
+  
  
 }while(a!=number)
-console.log("\n You gussed the number in "+ count + " Chances")
+
 
 
 
